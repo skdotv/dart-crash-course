9be01beb4cd313443497f75f3b2661421563d998
Vinod <vinodbs@geekyants.com>
2022-12-15 17:41:05 +0530

    const and final

---
+++ variables_and_data_types/bin/example1.dart
@@ -0,0 +1,5 @@
+void main(List<String> args) {
+  const name = 'Foo';
+  print(name);
+  // name = "Helo";
+}
---
+++ variables_and_data_types/bin/example2.dart
@@ -0,0 +1,6 @@
+void main(List<String> args) {
+  final age = [200, 300];
+  print(age);
+  age.removeAt(0);
+  print(age);
+}
